7ad9a855006348c55bbc7f757e2482cb4e07a798
Hanged Fish <94267867+hangedfish@users.noreply.github.com>
2022-05-19 00:01:57 +0800

    Update script.cpp

--- realloc_issue/script/script.cpp
+++ realloc_issue/script/script.cpp
@@ -1,24 +1,3 @@
-// #include <string>
-// #include <iostream>
-// #include <cstdio>
-
-// void sayHello(){
-//     std::cout << "hello!" << std::endl;
-// }
-
-// auto main(int argc, char* argv[]) -> int {
-//     static int rc = 0;
-// #if __wasi__
-//     if (argc == 0) rc += 1;
-//     std::string str{"hello form wasm"};
-// #else
-//     if (argc == 0) rc += 2;
-//     std::string str{"hello form cxx"};
-// #endif
-//     std::cout << str << std::endl;
-//     return 0;
-// }
-
 #include <stdio.h>
 #include <stdlib.h>
 
